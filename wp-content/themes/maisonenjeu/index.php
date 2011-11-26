c086b19d40494370a7e019f53ee4b08fa89d2137
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2011-11-26 14:57:57 +0100

    [dev] Ajouter un menu "Rubriques" pour les catégories

--- wp-content/themes/maisonenjeu/index.php
+++ wp-content/themes/maisonenjeu/index.php
@@ -15,6 +15,13 @@
 		
 		<div id="nav">
 			<?php wp_page_menu(array('show_home' => true)) ?>
+			
+			<div class="rubriques">
+			    <h2>Rubriques</h2>
+			    <ul>
+			        <?php wp_list_categories(array('title_li' => '')); ?> 
+			    </ul>
+			</div>
 		</div><!-- /#nav -->
 		
 		<div id="content">
