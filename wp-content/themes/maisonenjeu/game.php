8b4cc3a59650a0b41b9cf4e3d8e5b323a76ab53e
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-10-05 23:48:09 +0200

    [dev] Utiliser le plugin twikin pour la ludo

--- wp-content/themes/maisonenjeu/game.php
+++ wp-content/themes/maisonenjeu/game.php
@@ -1,39 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <title>Maison en Jeu</title>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="viewport" content="width=device-width" />
-        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
         <?php echo wp_head(); ?>
     </head>
     <body>
   	<div id="page">
-		<div id="header">
-			<h1 id="logo"><?php bloginfo( 'name' ); ?></h1>
-		</div><!-- /#header -->
-		
-		<div id="nav">
-			<?php wp_page_menu() ?>
-		</div><!-- /#nav -->
+		<?php get_header() ?>
 		
 		<div id="content">
 			<div id="texte">
 				<?php the_post(); ?>
-			    <?php echo the_game(); ?>
+				<?php echo do_shortcode('[game]'); ?>
 			</div><!-- /#texte -->
-			<div id="sidebar">
-				<?php echo gCalendar_nextEvent() ?>
-			</div><!-- /#sidebar -->
+			
+			<?php get_template_part('sidebar', 'right') ?>
 			<hr id="end-content" /> 
 		</div><!-- /#content -->
 		
-		<div id="footer">
-		    <ul>
-		        <li><a href="http://www.wordpress-fr.net/" title="Fièrement propulsé par Wordpress">WordPress</a></li>
-		        <li><?php wp_loginout(); ?></li>
-		    </ul>
-		</div><!-- /#footer -->
+		<?php get_footer() ?>
+		
 	</div><!-- /#page -->
 	<?php wp_footer(); ?>
   </body>
