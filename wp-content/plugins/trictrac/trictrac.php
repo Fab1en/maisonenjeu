2c7c6ccf663f0ecdb4e95129e3e38135a3910575
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2014-04-29 14:38:43 +0200

    [archive] Réécriture du plugin Trictrac (obsolette)

--- wp-content/plugins/trictrac/trictrac.php
+++ wp-content/plugins/trictrac/trictrac.php
@@ -1,77 +1,42 @@
 <?php
 /*
 Plugin Name: Trictrac
-Plugin URI: http://cabyres.com
 Description: Manage a game library hosted by TricTrac website (www.trictrac.net) 
-Version: 0.1
+Version: 1.0
 Author: Fabien Quatravaux
-Author URI: http://cabyres.com
+Author URI: http://fab1en.github.com/
 Text Domain: trictrac
 */
 
-include_once(plugin_dir_path(__FILE__).'includes/simple_html_dom.php');
+define('SCRIPT_DEBUG', true);
 
-add_action('init', 'trictrac_add_map_media', 1);
-function trictrac_add_map_media(){
-    if (function_exists('atmedia_register_media')){
-        atmedia_register_media(array('game' => array(
-            'title' => __('Ajouter un jeu'), 
-            'icon' => plugins_url( 'images/jeu.png', __FILE__ ),
-            'mime_type' => 'game/trictrac',
-            'add_new_form' => 'trictrac_add_new_form',
-        )));
+
+if(is_admin()){
+    add_filter( 'post_mime_types', 'trictrac_add_game_mime_type');
+    function trictrac_add_game_mime_type($mimes){
+        $mimes['game/trictrac'] = array(
+            'Jeux Trictrac',
+            'Gérer les jeux Trictrac',
+            _n_noop( 'Jeu <span class="count">(%s)</span>', 'Jeux <span class="count">(%s)</span>' )
+        );
+        
+        return $mimes;
     }
-}
-
-function trictrac_add_new_form(){
-    ?><div>
-        <p>
-            <label for="atmedia_title"><?php _e('Titre', 'trictrac') ?></label>
-            <input type="text" id="atmedia_title" name="atmedia_title"></input>
-        </p>
-        <p>
-            <input type="button" value="<?php _e('Chercher', 'trictrac') ?>" class="button" id="viewgamebutton" name="viewgamebutton">
-        </p>
-        
-        <input type="submit" value="<?php _e('Save') ?>" class="button" id="savegamebutton" name="savegamebutton">
-        
-    </div>
-    <?php
-}
-
-add_filter('media_upload_tabs', 'trictrac_add_tab');
-function trictrac_add_tab($tabs){
-    $tabs['trictrac'] = 'Trictrac';
-    return $tabs;
-}
-add_action("media_upload_trictrac", "trictrac_tab_render");
-function trictrac_tab_render(){
-    return wp_iframe( 'media_trictrac_import_bdd');
-}
-function media_trictrac_import_bdd(){
-    media_upload_header();
     
-    /*include_once(plugin_dir_path(__FILE__).'content/collection.db.php');
-    foreach($data as $game){
-        $id = wp_insert_attachment(array(
-	        'post_mime_type' => 'game/trictrac',
-	        'post_title' => addslashes(utf8_encode($game['nom'])),
-	        'guid' => 'http://www.trictrac.net/index.php3?id=jeux&rub=detail&inf=detail&jeu='.$game['id_trictrac'],
-	    ));
-	    update_post_meta($id, "atmedia", array(
-	        'miniature' => $game['vign'],
-	        'inventaire' => addslashes(utf8_encode($game['infos'])),
-	        'id_trictrac' => $game['id_trictrac'],
-	    ));
-	    echo addslashes(utf8_encode($game['nom']))."<br>\n";
-    }*/
-}
+    add_filter('wp_mime_type_icon', 'trictrac_game_icon', 10, 3);
+    function trictrac_game_icon($icon, $mime, $post_id){
+        if($mime == 'game/trictrac') return plugins_url('img/default.png', __FILE__);
+        return $icon;
+    }
+    
+} // is_admin
 
 add_action('wp_print_styles', 'trictrac_add_stylesheet');
 function trictrac_add_stylesheet(){
-    wp_enqueue_style('trictrac', plugins_url('/css/stylesheet.css',__FILE__));
+    wp_enqueue_style('trictrac', plugins_url('trictrac.css',__FILE__));
 }
 
+add_shortcode('ludotheque', 'trictrac_formatCollectionHTML');
 /**
 * Format the game library data in HTML.
 * Merge HTML template defined in config file with library data.
@@ -92,8 +57,6 @@ function trictrac_formatCollectionHTML(){
     'order' => 'ASC',
   ));
   
-  //print_r($games);
-  
   foreach($games as $game) :
     $metas =  get_post_meta($game->ID, "atmedia", true);
     $ludo .= '<li>';
@@ -110,84 +73,149 @@ function trictrac_formatCollectionHTML(){
   return $ludo;
 }
 
-/**
-* Format the game data in HTML.
-* Merge HTML template defined in config file with game data.
-* @param $game Array containing game data.
-* Data are indexed by keywords defined in config file.
-* @return The HTML content to display.
-*/
-function trictrac_formatGameHTML($game=array()){
-  
-  $o="<div class=\"ficheJeu\">";
-  $temp = "<h2 class=\"title\">#NOM#</h2>
-    <dl class=\"infosJeu\">
-      <dt>auteur</dt><dd>#AUTEUR#</dd>
-      <dt>editeur</dt><dd>#EDITEUR#</dd>
-      <dt>annee</dt><dd>#ANNEE#</dd>
-      <dt>genre</dt><dd>#GENRE#</dd>
-      <dt>public</dt><dd>#PUBLIC#</dd>
-      <dt>joueurs</dt><dd>#JOUEURS#</dd>
-      <dt>duree</dt><dd>#DUREE#</dd>
-    </dl>
-    <img src=\"#IMG#\"/>
-    </div>";
-    
-  // loop on keyword list
-  foreach (preg_split("/,/","nom,id_trictrac,auteur,editeur,annee,genre,public,joueurs,duree,img") as $keyword){
-	// replace the keyword by its value
-	$temp=preg_replace('/\#'.strtoupper($keyword).'\#/',utf8_encode($game[$keyword]),$temp);
-  }
-  $o.=$temp;
-  
-  // credit link
-  $o.="<div class=\"credits\">liste et informations hébergées par 
-  <a href=\"http://www.trictrac.net/index.php3?id=jeux&rub=detail&inf=detail&jeu=".$game['id_trictrac']."
-  \">Tric Trac</a></div>";
-  
-  return $o;
-}
-
-/**
-* Call Tric Trac remote page and parse HTML
-* to extract game data.
-*
-* @param $id_trictrac TricTrac game id
-* @return An array containing game data
-* Data are indexed by keyword.
-*/
-function trictrac_getGame($id_trictrac){
-  $urlFicheJeu="http://www.trictrac.net/index.php3?id=jeux&rub=detail&inf=detail";
-  $urlFicheJeu.="&jeu=".$id_trictrac;
-  
-  $ficheJeu=file_get_html($urlFicheJeu);
-  $jeu=array();
-  $ficheJeu=$ficheJeu->find('table', 14); 
-  if($ficheJeu == "") return "no data";
-  $jeu['nom']=$ficheJeu->find('tr', 0)->find('i', 0)->innertext;
-  
-  $infos=$ficheJeu->find('table', 0);
-  if($infos->find('tr', 13) == "") return "wrong ID: $id_trictrac";
-  
-  $jeu['auteur']=$infos->find('tr', 0)->find('b a', 0)->innertext;
-  $jeu['editeur']=$infos->find('tr', 2)->find('b a', 0)->innertext;
-  $jeu['annee']=$infos->find('tr', 6)->find('b a', 0)->innertext;
-  $jeu['genre']=$infos->find('tr', 7)->find('b a', 0)->innertext;
-  $jeu['public']=$infos->find('tr', 8)->find('b a', 0)->innertext;
-  $jeu['joueurs']=$infos->find('tr', 11)->find('b font', 0)->innertext;
-  $jeu['duree']=$infos->find('tr', 13)->find('b font', 0)->innertext;
-  $jeu['img']="http://www.trictrac.net/".$ficheJeu->find('tr', 2)->find('td', 0)->next_sibling()->find('img',0)->src;
-  $jeu['id_trictrac']=$id_trictrac;
-  
-  return $jeu;
-}
-
 function the_game(){
-    global $post;
-    $metas =  get_post_meta($post->ID, "atmedia", true);
-    echo trictrac_formatGameHTML(trictrac_getGame($metas['id_trictrac']));
+    
 }
 
-add_shortcode('ludotheque', 'trictrac_formatCollectionHTML');
+/**
+ * The Gallery shortcode.
+ *
+ * This implements the functionality of the Gallery Shortcode for displaying
+ * WordPress images on a post.
+ *
+ * @since 2.5.0
+ *
+ * @param array $attr Attributes of the shortcode.
+ * @return string HTML content to display gallery.
+ */
+function trictrac_gallery_shortcode($attr) {
+    return '';
+    
+	$post = get_post();
 
-?>
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+		// 'ids' is explicitly ordered, unless you specify otherwise.
+		if ( empty( $attr['orderby'] ) )
+			$attr['orderby'] = 'post__in';
+		$attr['include'] = $attr['ids'];
+	}
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr));
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$icontag = tag_escape($icontag);
+	$valid_tags = wp_kses_allowed_html( 'post' );
+	if ( ! isset( $valid_tags[ $itemtag ] ) )
+		$itemtag = 'dl';
+	if ( ! isset( $valid_tags[ $captiontag ] ) )
+		$captiontag = 'dd';
+	if ( ! isset( $valid_tags[ $icontag ] ) )
+		$icontag = 'dt';
+
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+		</style>
+		<!-- see gallery_shortcode() in wp-includes/media.php -->";
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+		$output .= "<{$itemtag} class='gallery-item'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />';
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+		</div>\n";
+
+	return $output;
+}
+
